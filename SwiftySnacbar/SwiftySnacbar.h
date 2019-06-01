542510341c392e696773910904f66e4a28f59187
mcz9mm <k.m.swim411@gmail.com>
2019-06-01 14:50:07 +0900

    create proj

---
+++ SwiftySnacbar/SwiftySnacbar.h
@@ -0,0 +1,19 @@
+//
+//  SwiftySnacbar.h
+//  SwiftySnacbar
+//
+//  Created by MataraiKaoru on 2019/06/01.
+//  Copyright © 2019 MataraiKaoru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftySnacbar.
+FOUNDATION_EXPORT double SwiftySnacbarVersionNumber;
+
+//! Project version string for SwiftySnacbar.
+FOUNDATION_EXPORT const unsigned char SwiftySnacbarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftySnacbar/PublicHeader.h>
+
+
---
+++ SwiftySnacbar/SwiftySnacbar.swift
@@ -0,0 +1,15 @@
+//
+//  SwiftySnacbar.swift
+//  SwiftySnacbar
+//
+//  Created by MataraiKaoru on 2019/06/01.
+//  Copyright © 2019 MataraiKaoru. All rights reserved.
+//
+
+import Foundation
+
+public class SwiftySnacbar: NSObject {
+    public func show() {
+        print("show")
+    }
+}
